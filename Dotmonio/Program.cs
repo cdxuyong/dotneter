7f3c3b711efd8b227082fa04470eb3bca5d59780
cdxuy <cdxuyong@qq.com>
2021-09-02 00:19:30 +0800

    fix:1

--- Dotmonio/Program.cs
+++ Dotmonio/Program.cs
@@ -7,6 +7,7 @@ namespace Dotmonio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!1");
         }
     }
 }
